9f1d4e3cd373980ca22c06d4e460266d4b1e2226
Brosaidon <jos.lidb3ck@gmail.com>
2026-05-12 10:46:48 +0200

    removed stuff

--- obj/Debug/net10.0/Market2.AssemblyInfo.cs
+++ obj/Debug/net10.0/Market2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Market2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd23a57c3c62c4ffa0c0695e327e92616d375df5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Market2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Market2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
